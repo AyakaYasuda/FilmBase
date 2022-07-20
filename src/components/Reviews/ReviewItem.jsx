7d4c01fe052c270f85c201e103fe42fa1621aaeb
Ayaka Yasuda <ayakayasuda@Ayakas-MacBook-Air.local>
2022-07-20 15:32:58 -0700

    added delete button

--- src/components/Reviews/ReviewItem.jsx
+++ src/components/Reviews/ReviewItem.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { useQuery } from 'react-query';
+import { useQuery, useQueryClient, useMutation } from 'react-query';
 import { Link } from 'react-router-dom';
-import * as api from '../../services/movies-api';
+import * as movieApi from '../../services/movies-api';
+import * as reviewApi from '../../services/reviews-api';
 
 const ReviewItem = ({ review }) => {
   const { uid, token } = useSelector((state) => state.users);
   const movieId = review.movie_id;
+  const queryClient = useQueryClient();
 
   const { data: movie } = useQuery(
     ['REVIEW_MOVIE', movieId],
-    () => api.getMovieById({ movieId, token }),
+    () => movieApi.getMovieById({ movieId, token }),
     { retry: false, enabled: !!movieId }
   );
+
+  const deleteReviewMutation = useMutation(reviewApi.deleteReview, {
+    onSuccess: () => {
+      queryClient.invalidateQueries(['MY_REVIEWS', uid]);
+      console.log('deleted');
+    },
+  });
+
+  const deleteHandler = () => {
+    deleteReviewMutation.mutate({ reviewId: review.review_id, token });
+  };
 
   return (
     <div>
@@ -23,7 +36,7 @@ const ReviewItem = ({ review }) => {
       <Link to={`/my-reviews/${uid}/edit/${review.review_id}`}>
         <button>edit</button>
       </Link>
-      <button>delete</button>
+      <button onClick={deleteHandler}>delete</button>
     </div>
   );
 };
