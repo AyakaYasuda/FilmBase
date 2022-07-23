6dec1f39339690c1cff97df07c23e4cc9b19c6d0
Ayaka Yasuda <ayakayasuda@Ayakas-Air.hitronhub.home>
2022-07-23 02:44:20 -0700

    styled MyReviews page and Reviews page

--- src/components/Reviews/ReviewItem.jsx
+++ src/components/Reviews/ReviewItem.jsx
@@ -5,7 +5,25 @@ import { Link } from 'react-router-dom';
 import * as movieApi from '../../services/movies-api';
 import * as reviewApi from '../../services/reviews-api';
 
+import { styled } from '@mui/material/styles';
 import LikeButton from '../Likes/LikeButton';
+import Rating from '@mui/material/Rating';
+import classes from './ReviewItem.module.scss';
+
+const StyledRating = styled(Rating)({
+  '& .MuiRating-iconFilled': {
+    color: '#ffe251',
+    fontSize: 30,
+  },
+  '& .MuiRating-iconHover': {
+    color: '#ffe251',
+    fontSize: 30,
+  },
+  '& .MuiRating-iconEmpty': {
+    color: '#ffe251',
+    fontSize: 30,
+  },
+});
 
 const ReviewItem = ({ review }) => {
   const { uid, token } = useSelector((state) => state.users);
@@ -29,20 +47,36 @@ const ReviewItem = ({ review }) => {
   };
 
   return (
-    <div>
-      <h1>{movie?.title}</h1>
-      <p>{review.comment}</p>
-      <p>{review.rate}</p>
-      <p>By {review.name}</p>
+    <div className={classes.review}>
+      <h3>{movie?.title}</h3>
+      <div className={classes.container}>
+        <div className={classes.rating}>
+          <StyledRating
+            max={10}
+            size="small"
+            value={review.rate}
+            precision={0.5}
+            readOnly
+          />
+          <h6>{review.rate}</h6>
+        </div>
+        <p>{review.comment}</p>
+        <h5>
+          <small>Reviewed By</small> {review.name}
+        </h5>
+      </div>
       {review.reviewer_id === uid && (
-        <>
+        <div className={classes.buttons}>
           <Link to={`/my-reviews/${uid}/edit/${review.review_id}`}>
             <button>edit</button>
           </Link>
           <button onClick={deleteHandler}>delete</button>
-        </>
+        </div>
       )}
-      <LikeButton reviewId={review.review_id} />
+
+      <div className={classes['like-button']}>
+        <LikeButton reviewId={review.review_id} />
+      </div>
     </div>
   );
 };
--- src/pages/MyReviews.jsx
+++ src/pages/MyReviews.jsx
@@ -4,6 +4,8 @@ import { useQuery } from 'react-query';
 import * as api from '../services/reviews-api';
 
 import ReviewItem from '../components/Reviews/ReviewItem';
+import LoadingSpinner from '../components/UI/LoadingSpinner';
+import classes from './MyReviews.module.scss';
 
 const MyReviews = () => {
   const { uid, token } = useSelector((state) => state.users);
@@ -23,15 +25,15 @@ const MyReviews = () => {
   );
 
   if (isLoading || isFetching) {
-    return (
-      <div className="section-container">
-        <p>Loading...</p>
-      </div>
-    );
+    return <LoadingSpinner />;
   }
+  //FIXME: error handling
 
   return (
-    <div className="section-container">
+    <div className={classes['my-reviews-container']}>
+      <h2>
+        <span>M</span>y Reviews
+      </h2>
       {myReviews.length !== 0 &&
         myReviews.map((review) => (
           <ReviewItem key={review.review_id} review={review} />
